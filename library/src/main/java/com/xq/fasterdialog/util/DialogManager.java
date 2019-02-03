d92b754cc9666b679cbae409b4801a2d148f5522
XQ <120363542@qq.com>
2019-02-03 23:35:37 +0800

    将DialogDelegateActivity移至DialogManager内部类

--- library/src/main/java/com/xq/fasterdialog/util/DialogManager.java
+++ library/src/main/java/com/xq/fasterdialog/util/DialogManager.java
@@ -1,6 +1,11 @@
 package com.xq.fasterdialog.util;
 
-import com.xq.fasterdialog.DialogDelegateActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import com.xq.fasterdialog.FasterDialog;
 import com.xq.fasterdialog.base.BaseDialog;
 
 public class DialogManager {
@@ -29,6 +34,42 @@ public class DialogManager {
 
     public static BaseDialog getDialog(){
         return dialog;
+    }
+
+    public static class DialogDelegateActivity extends AppCompatActivity {
+
+        private static DialogContextProvider contextProvider;
+
+        public static void startActivity(DialogContextProvider contextProvider){
+            DialogDelegateActivity.contextProvider = contextProvider;
+            Intent intent = new Intent(FasterDialog.getApp(), DialogDelegateActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            FasterDialog.getApp().startActivity(intent);
+        }
+
+        @Override
+        protected void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            BaseDialog dialog = contextProvider.showDialig(this);
+            dialog.addOnDismissListener(new BaseDialog.OnDialogDismissListener() {
+                @Override
+                public void onDismiss(BaseDialog dialog) {
+                    finish();
+                }
+            });
+        }
+
+        @Override
+        protected void onDestroy() {
+            super.onDestroy();
+            contextProvider = null;
+        }
+
+        public static interface DialogContextProvider{
+
+            public BaseDialog showDialig(Context context);
+
+        }
     }
 
 }
