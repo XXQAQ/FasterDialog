9518b3f386aafa1631d29d9ed9effad2a9055427
XXQAQ <120363542@qq.com>
2019-04-17 09:21:40 +0800

    实验性对导航键做优化

--- library/src/main/java/com/xq/fasterdialog/base/BaseDialog.java
+++ library/src/main/java/com/xq/fasterdialog/base/BaseDialog.java
@@ -81,7 +81,7 @@ public abstract class BaseDialog<T extends BaseDialog>{
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT)
         {
             window.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-//            window.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION, WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+            window.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION, WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
         }
 
         if (animatStyle != 0) window.setWindowAnimations(animatStyle);
@@ -188,7 +188,11 @@ public abstract class BaseDialog<T extends BaseDialog>{
 
         onCreate(null);
         onStart();
+
+        NavigationBarUtil.focusNotAle(dialog.getWindow());
         dialog.show();
+        NavigationBarUtil.hideNavigationBar(dialog.getWindow());
+        NavigationBarUtil.clearFocusNotAle(dialog.getWindow());
 
         if (autoDismissTime > 0)
             autoDismiss();
@@ -611,4 +615,54 @@ public abstract class BaseDialog<T extends BaseDialog>{
         }
 
     }
+
+    protected static class NavigationBarUtil {
+
+        /**
+         * 隐藏虚拟栏 ，显示的时候再隐藏掉
+         * @param window
+         */
+        static public void hideNavigationBar(final Window window) {
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+            window.getDecorView().setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
+                @Override
+                public void onSystemUiVisibilityChange(int visibility) {
+                    int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                            //布局位于状态栏下方
+                            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                            //全屏
+                            View.SYSTEM_UI_FLAG_FULLSCREEN |
+                            //隐藏导航栏
+                            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
+                    if (Build.VERSION.SDK_INT >= 19) {
+                        uiOptions |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+                    } else {
+                        uiOptions |= View.SYSTEM_UI_FLAG_LOW_PROFILE;
+                    }
+                    window.getDecorView().setSystemUiVisibility(uiOptions);
+                }
+            });
+        }
+
+        /**
+         * dialog 需要全屏的时候用，和clearFocusNotAle() 成对出现
+         * 在show 前调用  focusNotAle   show后调用clearFocusNotAle
+         * @param window
+         */
+        static public void focusNotAle(Window window) {
+            window.setFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                    WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE);
+        }
+
+        /**
+         * dialog 需要全屏的时候用，focusNotAle() 成对出现
+         * 在show 前调用  focusNotAle   show后调用clearFocusNotAle
+         * @param window
+         */
+        static public void clearFocusNotAle(Window window) {
+            window.clearFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE);
+        }
+    }
+
 }
